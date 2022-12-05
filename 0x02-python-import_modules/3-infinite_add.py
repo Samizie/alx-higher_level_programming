f24e6bdadb9ddf4470c7d9ceee0e03f111a2547b
Samizie <samilincoln@hotmail.com>
2022-12-04 16:48:04 -0800

    new 3a

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     argv = sys.argv[1:]
     argv_count = len(argv)
-    index =1 
+    index = 1
     res = 0
     while index <= argv_count:
         res += int(sys.argv[index])
