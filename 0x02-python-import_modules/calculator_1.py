fa25a0686624cd9a0e26a6c2da46ca7fdd5a244e
Samizie <samilincoln@hotmail.com>
2022-12-04 16:10:07 -0800

    new 1

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -21,7 +21,29 @@ def sub(a, b):
 
     Returns:
         The return value. a - b)
-
     """
+    return (a - b)
 
-                             return (a - b)
+def mul(a, b):
+    """My multiplication function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a * b
+    """
+    return (a * b)
+
+def div(a, b):
+    """My division function
+
+    Args:
+        a: first integer
+        b: second integer
+
+    Returns:
+        The return value. a / b
+    """
+    return int(a / b)
