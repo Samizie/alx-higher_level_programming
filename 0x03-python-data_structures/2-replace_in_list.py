70a96755329a5d7fac3c5bc4a47b030fa28329b0
Samizie <samilincoln@hotmail.com>
2022-12-05 05:40:27 -0800

    replace 4

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def replace_in_list(my_list, idx, new_element):
     new_list = my_list
     x = len(my_list)
     if idx >= 0 or idx < x:
-        new_list[idx] = element
+        new_list[idx] = new_element
     return my_list
