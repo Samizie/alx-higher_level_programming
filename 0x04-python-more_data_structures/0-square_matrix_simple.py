774b54e9bb2e4d316d7e240bf8cd27283453685f
Samizie <samilincoln@hotmail.com>
2022-12-07 03:52:00 -0800

    matrix 3

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,4 +4,4 @@ def square_matrix_simple(matrix=[]):
     for row in matrix:
         sub_matrix = map(lambda num: num**2, row)
         response.append(list(sub_matrix))
-    return response 
+    return response
