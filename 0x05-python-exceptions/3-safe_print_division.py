68c03f21cda1f19d70b311a592f9ad8cc367e284
Samizie <samilincoln@hotmail.com>
2022-12-20 06:12:04 -0800

    more adjustment on thee error tpe

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -6,7 +6,7 @@ def safe_print_division(a, b):
     """
     try:
         res = a / b
-    except(TypeError, ValueError):
+    except (TypeError, ValueError):
         res = None
     finally:
         print("Inside result: {}".format(res))
