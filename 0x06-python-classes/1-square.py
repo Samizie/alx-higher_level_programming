201dafc41a270cd587c228b22591ba4430c3bc6e
Samizie <samilincoln@hotmail.com>
2022-12-20 17:24:34 -0800

    01 amends 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
+
+"""Defines a class square."""
+
+
 class Square:
+    """Represent a square."""
+
     def __init__(self, size):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the neew square
+        """
         self.__size = size
