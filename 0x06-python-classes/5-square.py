c854b3b3b1768fd91fe6f429254942a5e0bcc6ca
Samizie <samilincoln@hotmail.com>
2022-12-20 17:55:40 -0800

    05 amends 3

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -6,22 +6,22 @@
 class Square:
     """Represent a square."""
 
-    def __init__(self, size=0):
+    def __init__(self, size):
         """Initialize a new square.
+        
         Args:
             size (int): The size of the new square.
         """
-        self.__size = size
+        self.size = size
 
-     # Property
      @property
      def size(self):
+         """Get/set the current size of the square."""
          return self.__size
 
-     # Setter modifies
      @size.setter
      def size(self, value):
-         if type(value) != int:
+         if not isinstance(value, int):
              raise TypeError('size must be an integer')
          elif value < 0:
              raise ValueError('size must be >= 0')
@@ -35,7 +35,7 @@ class Square:
     def my_print(self):
         """Print the square with the # character."""
         for i in range(0, self.__size):
-            [print("#", end="") for  in range (self.__size)]
+            [print("#", end="") for j in range (self.__size)]
             print("")
 
         if self.__size == 0:
