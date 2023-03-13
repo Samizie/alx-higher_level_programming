81cd83f9f124214ae5845c2e0f670dca4e28febb
Samizie <samilincoln@hotmail.com>
2023-03-13 13:04:41 -0700

    shorter and better on second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,20 +1,2 @@
 #!/usr/bin/node
-
-function second (myArray) {
-  if (myArray.length === 2 || myArray.length === 3) { return (0); }
-
-  let max = myArray[2];
-  let secondMax = myArray[3];
-
-  for (let i = 2; i < myArray.length; i++) {
-    if (myArray[i] > max) {
-      secondMax = max;
-      max = myArray[i];
-    } else if (myArray[i] > secondMax && myArray[i] < max) {
-      secondMax = myArray[i];
-    }
-  }
-  return (secondMax);
-}
-
-console.log(second(process.argv));
+console.log(process.argv.length < 4 ? 0 : process.argv.slice(2).sort((a, b) => b - a)[1]);
