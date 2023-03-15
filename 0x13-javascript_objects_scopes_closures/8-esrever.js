fc88ad677efc4717e6f0568a7bb4e62c767b7dda
Samizie <samilincoln@hotmail.com>
2023-03-14 18:18:16 -0700

    8 esrever 2

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,11 @@
 #!/usr/bin/node
-// reverses a list
+// function that returns the reversed version of a list
+
 exports.esrever = function (list) {
-  return list.sort(() => 1);
+
+  let rlist = [];
+  while (list.length > 0) {
+    rlist.push(list.pop());
+  }
+  return rlist;
 };
